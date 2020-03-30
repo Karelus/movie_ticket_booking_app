c18c34b605a27fccff8a0b1346b6597961e58035
Karel Haapasalo <karel.haapasalo@hotmail.com>
2020-03-30 19:38:05 +0300

    modified js file

--- movieTicketApp/src/movieTicketApp/Tickets.java
+++ movieTicketApp/src/movieTicketApp/Tickets.java
@@ -9,23 +9,39 @@ public class Tickets {
 	public static Scanner reader = new Scanner(System.in);
 	public static void main(String[] args) {
 		
-		System.out.println("Please enter your name: ");
-		String userName = reader.nextLine();
-		System.out.println("Please also enter your age: ");
-		int userAge = reader.nextInt();
+		String[] movies = {"The Martian", "Interstellar", "Deadpool 2", "Sonic the Hedgehog", "Avatar"};
+		int[] seatsAvailable = {200, 123, 44, 83, 159};
+		// int[] ageRating = {12, 12, 16, 7, 13};
 		
-		String[] movies = {"1: The Martian", "2: Interstellar", "3: Deadpool 2", "4: Sonic the Hedgehog", "5: Avatar"};
+		//Asks for user input
+		String userName = enterName();
+		int userAge = enterAge();
 		
+		// Prints all movies 
 		System.out.println("Here are the movies currently playing: ");
-		for (String movie : movies) {
-			System.out.println(movie);
+		for (int i = 0, j = 1; i < movies.length; i++, j++) {
+			System.out.print(j + ": ");
+			System.out.print(movies[i]);
+			System.out.println();
 		}
 		
 		System.out.println("What movie would you like to book tickets for?(1, 2, 3, 4, 5): ");
-		int selectedMovie = reader.nextInt();
-		
-		
-		System.out.println("Hi "+userName+", you chose the movie number 3, which is "+movies[2]+" and it has an age rating of 18. Luckily you are "+userAge+" years old!");
+		int selectedMovie = reader.nextInt();		
+		System.out.println("How many seats would you like to book?");
+	}
+	
+	//Methods
+	
+	public static String enterName() {
+		System.out.println("Please enter your name: ");
+		String userName = reader.nextLine();
+		return userName;
+	}
+	
+	public static int enterAge() {
+		System.out.println("Please also enter your age: ");
+		int userAge = reader.nextInt();
+		return userAge;
 	}
 
 }
